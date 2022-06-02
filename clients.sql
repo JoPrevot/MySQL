2b11476e8c6a4489cc2ee84bea41d76ed320a774
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-02 11:32:37 +0200

    Sélection de la DB

--- clients.sql
+++ clients.sql
@@ -1,3 +1,17 @@
--- 1. Création de la base de données
+-- Authentification/connexion
+>mysql -u root -- Si l'utilisateur n'a pas de MDP
+>mysql -u root -p -- Si l'utilisateur a un MDP
 
--- 2. Création de la table clients
+-- 1. Lister toutes les base de données
+
+SHOW DATABASES;
+
+-- 2. Création de la base de données
+
+CREATE DATABASE Ventes;
+
+-- 3. Utiliser/sélectionner la base de données
+
+USE ventes;
+
+-- 4. Création de la table clients
