f52e4e686f5c86a0e70e24fb41b1e65eab69977b
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-03 10:27:05 +0200

    Ajout des conditions IN et BETWEEN

--- sales.sql
+++ sales.sql
@@ -1,6 +1,9 @@
+-- 1.1 Créer une base de données appelée sales
+
 CREATE DATABASE Sales;
 
 USE Sales;
+ -- 1.2 Créer une table appeléetelephonesavec les colonnes suivantes ...
 
 CREATE TABLE telephones(
 id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -9,6 +12,8 @@ manufacturer VARCHAR(30) NOT NULL,
 price FLOAT(4,2) NOT NULL,
 units_sold INTEGER(10) NOT NULL
 );
+
+-- 1.3 Insérer les données suivantes
 
 INSERT INTO telephones(name,manufacturer,price,units_sold) values
 ("N1280","Nokia",199,1925),
@@ -21,10 +26,26 @@ INSERT INTO telephones(name,manufacturer,price,units_sold) values
 ("Galaxy Note 20","Samsung",1100,10353)
 ;
 
+-- 2.1 Ecrire une requête pour liste tous les modèles de téléphones
+
 SELECT * FROM telephones;
+
+-- 2.2 Ecrire une requête pour lister tous les telephone(name, manufacturer) de tous lestéléphone dont le prix est supérieur à 200€
 
 SELECT name,manufacturer FROM telephones WHERE price>200;
 
+-- 2.3 Ecrire une requête pour lister tous les telephone(name,manufacturer) de tous lestéléphone dont le prix est varie entre 150 et 200€
+
 SELECT name,manufacturer FROM telephones WHERE price>=150 AND price<=200;
 
+-- On peut aussi écrire un intervalle avec la fonction <BETWEEN> qui évite les erreurs de syntaxe sur les conditions:
+
+SELECT name,manufacturer FROM telephones WHERE price BETWEEN 150 AND 200;
+
+-- 2.4 Lister tous les téléphones de marque samsung et Apple
+
 SELECT * FROM telephones WHERE manufacturer="Apple" OR manufacturer="Samsung";
+
+-- On peut regrouper toutes les conditions ici avec <IN> plutôt que d'utiliser des OR :
+
+SELECT * FROM `telephones` WHERE manufacturer IN("Apple","Samsung");
