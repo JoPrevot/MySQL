a9dbf8f1d102cb76ff6d8e9ddb35d677cf8ceb91
Jordan Prevot <jordan.prevot59@gmail.com>
2022-06-04 10:57:10 +0200

    Exo perso partie 4

--- exercice_aymeric_auberton.sql
+++ exercice_aymeric_auberton.sql
@@ -108,3 +108,82 @@ ORDER BY note DESC
 LIMIT 3 OFFSET 0
 ;
 
+-- IV. QUATRIEME PARTIE
+
+-- Q1 : Quelle requête utiliser pour retrouver la ville qui possède les coordonnées GPS suivantes : 48.66913724637683, 1.87586057971015 ?
+
+SELECT lpecom_cities.name
+FROM lpecom_cities
+WHERE gps_lat = "48.66913724637683" AND gps_lng = "1.87586057971015"
+;
+
+-- Q2 : Sans jointure, quelle requête utiliser pour calculer le nombre de villes que compte le département de l'Essonne ?
+
+SELECT count(*)
+FROM lpecom_cities
+WHERE department_code = 71
+;
+
+-- Q3 : Sans jointure, quelle requête utiliser pour calculer le nombre de villes en Île-de-France se terminant par "-le-Roi" ?
+
+SELECT count(*)
+FROM lpecom_cities
+WHERE name LIKE "%-le-Roi"
+;
+
+-- Q4 : Combien de villes possèdent le code postal (zip_code) 77320 ? Renommez la colonne de résultat n_cities.
+
+SELECT count(*) AS n_cities
+FROM lpecom_cities
+WHERE zip_code = 77320
+;
+
+-- Q5 : Sans jointure, quelle requête utiliser pour calculer le nombre de villes commençant par "Saint-" en Seine-et-Marne ?
+
+SELECT count(*)
+FROM lpecom_cities
+WHERE name LIKE "Saint-%"
+AND department_code = 77
+;
+
+-- Q6 : Quelles villes possèdent un code postal (zip_code) compris entre 77210 et 77810 ?
+
+SELECT name
+FROM lpecom_cities
+WHERE zip_code BETWEEN 77210 AND 77810
+;
+
+-- Q7 : Sans jointure, quelles sont les deux villes de Seine-et-Marne à avoir le code postal (zip_code) le plus grand ?
+
+SELECT name
+FROM lpecom_cities
+WHERE department_code = 77
+ORDER BY zip_code DESC
+LIMIT 2
+;
+
+-- Q8 : Quel est le code postal (zip_code) le plus grand de la table lpecom_cities ?
+
+SELECT MAX(zip_code)
+FROM lpecom_cities
+;
+
+-- Q9 : Avec un seul WHERE et aucun OR, quelle est la requête permettant d'afficher les départements des régions ayant le code suivant : 
+-- 75, 27, 53, 84 et 93 ? Le résultat doit afficher le nom du département ainsi que le nom et le slug de la région associée.
+
+SELECT lpecom_departments.name, lpecom_regions.name, lpecom_regions.slug
+FROM lpecom_departments
+JOIN lpecom_regions
+ON  lpecom_departments.code = lpecom_regions.code
+WHERE lpecom_regions.code IN (75,27,53,84,93)
+;
+
+-- Q10 : Quelle requête utiliser pour obtenir en résultat, les noms de la région, 
+-- du département et de chaque ville du département ayant pour code 77 ?
+
+SELECT r.name AS Region,d.name AS Departement, c.name As Ville
+FROM lpecom_cities c 
+JOIN lpecom_departments d ON c.department_code = d.code
+JOIN lpecom_regions r ON d.region_code = r.code
+WHERE d.code = 77
+;
